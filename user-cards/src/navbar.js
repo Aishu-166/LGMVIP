352dc40052ecbc04adea04ab9c052b78dc55bd8c
Anmol More <aishwaryagandhi166@gmail.com>
2022-05-01 00:15:58 +0530

    Task 2 update

---
+++ user-cards/src/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+const navbar= () =>{
+  return (
+  <div>
+    <li>
+      <ul>Get Users</ul>
+    </li>
+    
+  </div>
+  );
+}
+export default navbar;
